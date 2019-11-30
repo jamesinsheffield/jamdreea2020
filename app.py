59e3dd3d82d08da71f09f899945fe845e8460c2e
James <jamesinsheffield@hotmail.com>
2019-11-30 22:52:11 +0000

    text

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/flights-out')
+def flights_out():
+    return render_template('flights-out.html')
+
+@app.route('/flights-return')
+def flights_return():
+    return render_template('flights-return.html')
+
 if __name__ == "__main__":
     app.run()
